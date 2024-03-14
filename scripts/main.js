16696ed36fe9682ce3e25c01828b1ac23ed8fd2c
LOStHiFi <wjc15034287458@gmail.com>
2024-03-14 20:32:57 +0800

    Update Personal Website

--- scripts/main.js
+++ scripts/main.js
@@ -8,23 +8,3 @@ myImage.onclick = function () {
     myImage.setAttribute("src", "images/Minimal Cat.png");
   }
 };
-
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-  let myName = prompt("Enter Your Name");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = "Hope you also love the Cat, " + myName;
-}
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem("name");
-  myHeading.textContent = "Hope you also love the Cat, " + storedName;
-}
-
-myButton.onclick = function () {
-  setUserName();
-};
